2666589ed4046db511c252c8c20dc8db4b2c2f57
Sahand Pourjavad <spourjavad2012@gmail.com>
2022-04-03 08:21:01 +0430

    Added cors

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const config = require('config');
 const express = require('express');
+const { default: helmet } = require('helmet');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const comments = require('./routes/comments');
+const cors = require('cors');
 
 mongoose
 	.connect(config.get('db'))
 	.then(() => console.log('Connected to: ' + config.get('db')))
 	.catch((err) => console.log(err));
 
+app.use(cors({ origin: '*' }));
 app.use(express.json());
+app.use(helmet());
 app.use('/api/comments', comments);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
